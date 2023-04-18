51c607dd0e4f63b7cae871cdf4de138bc10440e7
Андрей Петров <mrandrushik@gmail.com>
2023-04-18 17:50:00 +0300

    edit bite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
-import svgr from 'vite-plugin-svgr';
 import Pages from 'vite-plugin-pages';
 import generateSitemap from 'vite-plugin-pages-sitemap';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
