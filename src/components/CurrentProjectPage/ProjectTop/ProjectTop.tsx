956472c3f181533d822b60b982b250fc91e3e961
Андрей Петров <mrandrushik@gmail.com>
2023-04-20 18:24:33 +0300

    attempt to change project img position

--- src/components/CurrentProjectPage/ProjectTop/ProjectTop.tsx
+++ src/components/CurrentProjectPage/ProjectTop/ProjectTop.tsx
@@ -6,53 +6,53 @@ import { ProjectType } from 'types/types';
 import cls from './ProjectTop.module.scss';
 
 type ProjectTopProps = Pick<
-    ProjectType,
-    | 'app_store_link'
-    | 'google_play_link'
-    | 'web_link'
-    | 'title'
-    | 'short_description'
-    | 'header_image'
-    | 'bg_text_color'
-    | 'bg_color'
+  ProjectType,
+  | 'app_store_link'
+  | 'google_play_link'
+  | 'web_link'
+  | 'title'
+  | 'short_description'
+  | 'header_image'
+  | 'bg_text_color'
+  | 'bg_color'
 >;
 
 export const ProjectTop = ({
-    app_store_link,
-    google_play_link,
-    web_link,
-    title,
-    short_description,
-    bg_color,
-    header_image,
-    bg_text_color,
+  app_store_link,
+  google_play_link,
+  web_link,
+  title,
+  short_description,
+  bg_color,
+  header_image,
+  bg_text_color,
 }: ProjectTopProps) => {
-    return (
-        <section style={{ background: bg_color }} className={cn(cls.ProjectTop, {}, [])}>
-            <div className={cls.block}>
-                <div style={{ color: bg_text_color }} className={cls.content}>
-                    <h1 className={cls.title}>{title}</h1>
-                    <p className={cls.text}>{short_description}</p>
-                    <div className={cls.linksWrapper}>
-                        {app_store_link && (
-                            <a href={app_store_link} target='blank' className={cls.appLink}>
-                                <AppStore className={`${cls.appleIcon} ${cls.icon}`} />
-                            </a>
-                        )}
-                        {google_play_link && (
-                            <a href={google_play_link} target='blank' className={cls.appLink}>
-                                <img className={`${cls.googleIcon} ${cls.icon}`} src={GooglePlay} alt='googlePlay' />
-                            </a>
-                        )}
-                        {web_link && (
-                            <a href={web_link} target='blank' className={cls.appLink}>
-                                <WebApp className={`${cls.webIcon} ${cls.icon}`} />
-                            </a>
-                        )}
-                    </div>
-                </div>
-                <img loading='lazy' className={cls.img} src={header_image} alt='Background' />
-            </div>
-        </section>
-    );
+  return (
+    <section style={{ background: bg_color }} className={cn(cls.ProjectTop, {}, [])}>
+      <div className={cls.block}>
+        <div style={{ color: bg_text_color }} className={cls.content}>
+          <h1 className={cls.title}>{title}</h1>
+          <p className={cls.text}>{short_description}</p>
+          <div className={cls.linksWrapper}>
+            {app_store_link && (
+              <a href={app_store_link} target='blank' className={cls.appLink}>
+                <AppStore className={`${cls.appleIcon} ${cls.icon}`} />
+              </a>
+            )}
+            {google_play_link && (
+              <a href={google_play_link} target='blank' className={cls.appLink}>
+                <img className={`${cls.googleIcon} ${cls.icon}`} src={GooglePlay} alt='googlePlay' />
+              </a>
+            )}
+            {web_link && (
+              <a href={web_link} target='blank' className={cls.appLink}>
+                <WebApp className={`${cls.webIcon} ${cls.icon}`} />
+              </a>
+            )}
+          </div>
+        </div>
+        {/* <img loading='lazy' className={cls.img} src={header_image} alt='Background' /> */}
+      </div>
+    </section>
+  );
 };
